e7ff4ac69489ebceeb3e5b797cf7dea31da56d36
SouvikPaul1998 <souvikkaka0@gmail.com>
2025-11-12 23:50:20 +0530

    commit 1

---
+++ Microservicesshoppingapp/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingUtilities;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal? Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product
+            //Create product entity from command
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            //save to database
+            //return Createproduct Result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
